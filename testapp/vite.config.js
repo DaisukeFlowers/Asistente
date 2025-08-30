6be9cffd19183fde0222f2760d70b8bc4c064c28
d-mendz <107445442+d-mendz@users.noreply.github.com>
2025-08-29 18:01:02 -0600

    Update vite.config.js

--- testapp/vite.config.js
+++ testapp/vite.config.js
@@ -1,19 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // Todo lo que empiece con /api en dev
-      // se envía a tu backend local (ajusta el puerto si usas otro)
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        // opcional: reescribe /api -> /api si tu back ya sirve bajo /api
-        // rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-    },
-  },
-})
+export default defineConfig({ plugins: [react()] })
